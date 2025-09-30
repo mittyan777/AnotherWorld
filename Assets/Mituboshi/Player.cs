4973cfee6a74a756fde9d11ec606ca21c648127c
mittyan777 <C3289@oic.jp>
2025-09-30 17:57:04 +0900

    プレイヤーの移動処理とGameManagerの作成

---
+++ Assets/Mituboshi/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Animator animator;  
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        if(Input.GetKey("w"))
+        {
+            transform.position += transform.forward * 5 * Time.deltaTime;
+            animator.SetBool("walk", true);
+        }
+        else { animator.SetBool("walk", false); }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * 5 * Time.deltaTime;
+            animator.SetBool("back", true);
+        }
+        else { animator.SetBool("back", false); }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * 5 * Time.deltaTime;
+            animator.SetBool("left", true);
+        }
+        else { animator.SetBool("left", false); }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * 5 * Time.deltaTime;
+            animator.SetBool("right", true);
+        }
+        else { animator.SetBool("right", false); }
+
+        
+    }
+}
