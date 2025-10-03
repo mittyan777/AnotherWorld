e2ce6916f80a67d1eb974271a57987dcd39be861
hikidoharuki <hharu05@icloud.com>
2025-10-03 17:53:23 +0900

    hikido/player
    testpush

--- Assets/Hikido/Scripts/hgikidiiiiiiiiiiii.cs
+++ Assets/Hikido/Scripts/hgikidiiiiiiiiiiii.cs
@@ -7,7 +7,7 @@ public class hgikidiiiiiiiiiiii : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //hello
     }
 
     // Update is called once per frame
