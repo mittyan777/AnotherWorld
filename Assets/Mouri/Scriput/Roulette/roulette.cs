15c2ae43c2b7dc90dc5932d3ea519c46ae9fc523
Min-itagaki2 <131222959+Min-itagaki2@users.noreply.github.com>
2025-10-03 21:49:41 +0900

    ルーレットの使用時プレイヤーの操作
    ルーレットの使用時プレイヤー操作を一時的に停止させて、スロットのボタンを押したときに少しの間があり、プレイヤー操作が再開される

--- Assets/Mouri/Scriput/Roulette/roulette.cs
+++ Assets/Mouri/Scriput/Roulette/roulette.cs
@@ -7,7 +7,14 @@ public class roulette : MonoBehaviour
 {
     [SerializeField]int power = 0;
     bool powerslot = true;
+
     [SerializeField] Text powerslot_text;
+
+    [SerializeField] Player_mitubosi player;
+
+    [SerializeField] GameObject slotUI;
+
+    [SerializeField] float tienn;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +24,13 @@ public class roulette : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (powerslot)
+        {
+            power = Random.Range(1, 100);
+
+        }
+
+        powerslot_text.text=power.ToString();
     }
     private void FixedUpdate()
     {
@@ -26,8 +39,27 @@ public class roulette : MonoBehaviour
        
         powerslot_text.text = ($"{power}");
     }
-   public void stopslot()
+    public void stopslot()
     {
         powerslot = false;
+
+        StartCoroutine(CloseSlotAfterDelay(tienn));
     }
+
+    IEnumerator CloseSlotAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        
+        slotUI.SetActive(false);
+        player.Slotstop();
+    }
+
+    public void StartSlot()
+    {
+        power = 0;
+        powerslot = true;
+        powerslot_text.text = "0";
+        slotUI.SetActive(true);
+    }
+
 }
