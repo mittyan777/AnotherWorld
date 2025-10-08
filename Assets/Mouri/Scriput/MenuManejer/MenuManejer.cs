0660ed744e06b7e2fe701d600060e17688eb54ab
Min-itagaki2 <131222959+Min-itagaki2@users.noreply.github.com>
2025-10-08 20:05:19 +0900

    ルーレットマネージャー？
    Escキーを押した場合ポーズ画面が開く。ポーズ画面を開いているときにEscキーもう一度押した場合はポーズ画面が閉じる

---
+++ Assets/Mouri/Scriput/MenuManejer/MenuManejer.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManejer : MonoBehaviour
+{
+    //メニューのルートオブジェクト(全体をまとめた Canvas など)
+    public GameObject menuUI;
+
+    // 初期状態を非表示にするかどうか
+    public bool startMenuHidden = true;
+
+    // ポーズ中かどうかを管理
+    public bool isPaused = false;
+
+    //public AudioSource Audio;//追加
+
+    //public AudioSource Audio2;
+
+    void Start()
+    {
+        // 初期状態でメニューを非表示にする
+        if (menuUI != null)
+        {
+            menuUI.SetActive(!startMenuHidden);
+        }
+        if (startMenuHidden)
+        {
+            ResumeGame();
+        }
+    }
+
+    void Update()
+    {
+        // Escキーでメニューを切り替える
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+
+                //PauseAudio();//追加
+            }
+            else
+            {
+                PauseGame();
+
+                //PauseAudio();//追加
+            }
+
+        }
+
+
+    }
+
+    public void PauseGame()
+    {
+        if (menuUI != null)
+        {
+            menuUI.SetActive(true); // メニューを表示
+        }
+        Time.timeScale = 0f; // ゲームを停止
+        isPaused = true;
+    }
+    /*public void PauseAudio()  //PauseAudioの中の意味//追加
+    {
+        if (Audio != null)//もしも
+        {
+            Audio.Play();　//追加
+        }
+        if (isPaused)//追加
+        {
+            Audio.Pause();//追加
+        }
+
+
+
+    }
+    */
+    public void ResumeGame()
+    {
+        if (menuUI != null)
+        {
+            menuUI.SetActive(false); // メニューを非表示
+        }
+        Time.timeScale = 1f; // ゲームを再開
+        isPaused = false;
+    }
+}
