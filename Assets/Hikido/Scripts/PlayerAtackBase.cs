9161c4482072e299e7cec12e2d689d29389c23b0
hikidoharuki <hharu05@icloud.com>
2025-10-24 17:55:39 +0900

    hikido/Player_atackMove2
    プレイヤーのベース作成

--- Assets/Hikido/Scripts/PlayerAtackBase.cs
+++ Assets/Hikido/Scripts/PlayerAtackBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
@@ -5,9 +6,14 @@ using UnityEngine;
 
 public class PlayerAtackBase : MonoBehaviour
 {
-    Animator _animator;
-   
+    [SerializeField] Animator _animator;
 
+    /// <summary> /// レンジ構造体 /// </summary>
+    struct PlayerRange
+    {
+        [SerializeField] int mangicMaxRange;
+        [SerializeField] int archerMaxRange;
+    }
 
     void Start()
     {
@@ -16,7 +22,7 @@ public class PlayerAtackBase : MonoBehaviour
 
     void Update()
     {
-       
+        Avoidance();
     }
 
     /// <summary> /// 各職種の特殊攻撃 /// </summary>
@@ -26,14 +32,15 @@ public class PlayerAtackBase : MonoBehaviour
     }
 
     /// <summary> s/// 全職種共通の攻撃処理 /// </summary>
-    private void Atack_Normal()
+    protected virtual void Atack_Normal()
     {
         //TODO:マウス左クリックで通常攻撃
         if (Input.GetMouseButtonDown(0))
         {
             //攻撃のダメージ処理
+            
+            //TODO：攻撃アニメーション
 
-            //TODO：アニメーション処理
         }
         
     }
@@ -47,17 +54,23 @@ public class PlayerAtackBase : MonoBehaviour
 
         //shiftキー入力判定
         bool _isShiftKey = Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift);
+        bool _isSpaceKey = Input.GetKeyDown(KeyCode.Space);
 
         UnityEngine.Vector3 _inputDirection = new UnityEngine.Vector3(_inputHorizontal, 0f, _inputVertical).normalized;
 
         //Shift + 方向キーでの回避
-        if(_isShiftKey  && _inputDirection.magnitude > 0.1f)
+        if (_isShiftKey && _inputDirection.magnitude > 0.1f || _isSpaceKey && _inputDirection.magnitude > 0.1f)
         {
             UnityEngine.Quaternion targetRotation = UnityEngine.Quaternion.LookRotation(_inputDirection);
             transform.rotation = targetRotation;
+            _animator.SetBool("avoidance", true);
+            Debug.Log("回避方向");
         }
-
-        //TODO:回避アニメーション(アニメーター処理）
+        else
+        {
+            _animator.SetBool("avoidance", false);
+        }
+        
         
     }
 
