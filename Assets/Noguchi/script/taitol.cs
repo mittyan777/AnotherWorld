32c02124371446f8dd125016302493b0a8b6d489
Noguchi0720 <C3229@oic.jp>
2025-11-11 15:10:55 +0900

    タイトル編集

--- Assets/Noguchi/script/taitol.cs
+++ Assets/Noguchi/script/taitol.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Canvas : MonoBehaviour
 {
     [SerializeField] private Button buttonA;
     [SerializeField] private Button buttonB;
     [SerializeField] private Image fadeImage; // 黒いフェード用イメージ
+     [SerializeField] private string SceneName = "Scene";
 
     private bool isFading = false;
 
@@ -14,25 +16,17 @@ public class Canvas : MonoBehaviour
     {
         // ボタンA
         if (buttonA != null)
-        {
-            buttonA.onClick.AddListener(() => OnButtonClicked("A"));
-            Debug.Log("ボタンAの監視を開始しました。");
-        }
+        {   buttonA.onClick.AddListener(() => OnButtonClicked("A"));
+            Debug.Log("ボタンAの監視を開始しました。");}
         else
-        {
-            Debug.LogError("buttonA が設定されていません！");
-        }
+        {Debug.LogError("buttonA が設定されていません！");}
 
         // ボタンB
         if (buttonB != null)
-        {
-            buttonB.onClick.AddListener(() => OnButtonClicked("B"));
-            Debug.Log("ボタンBの監視を開始しました。");
-        }
+        {   buttonB.onClick.AddListener(() => OnButtonClicked("B"));
+            Debug.Log("ボタンBの監視を開始しました。");}
         else
-        {
-            Debug.LogError("buttonB が設定されていません！");
-        }
+        {Debug.LogError("buttonB が設定されていません！");}
 
         // フェードイメージ初期化
         if (fadeImage != null)
@@ -48,16 +42,14 @@ public class Canvas : MonoBehaviour
         Debug.Log($"ボタン{buttonName} が押されました！");
 
         if (buttonName == "B" && !isFading)
-        {
-            StartCoroutine(FadeOutAndQuit());
-        }
+        {StartCoroutine(FadeOutAndQuit());}
     }
 
     private IEnumerator FadeOutAndQuit()
     {
         isFading = true;
 
-        float duration = 2f; // フェードにかかる時間
+        float duration = 2f; // フェード時間
         float time = 0f;
 
         while (time < duration)
@@ -75,21 +67,22 @@ public class Canvas : MonoBehaviour
             yield return null;
         }
 
-        Debug.Log("フェードアウト完了。ゲームを終了します。");
+        Debug.Log($"フェードアウト完了。シーン「{SceneName}」へ移動します。");
+        SceneManager.LoadScene(SceneName);
 
-#if UNITY_EDITOR
+        #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false; // エディタ実行停止
-#else
+        #else
         Application.Quit(); // ビルド後の実行ファイル終了
-#endif
+        #endif
     }
 
     void Update()
     {
         if (buttonA != null && !buttonA.interactable)
-            Debug.Log("ボタンA は無効状態で押せません。");
+           {Debug.Log("ボタンA は無効状態で押せません。");}
 
         if (buttonB != null && !buttonB.interactable)
-            Debug.Log("ボタンB は無効状態で押せません。");
+           {Debug.Log("ボタンB は無効状態で押せません。");}
     }
 }
