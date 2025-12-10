d7aaf2431a2c7f09e5a285b12a8adfa8bdcfd6a9
Min-itagaki2 <131222959+Min-itagaki2@users.noreply.github.com>
2025-12-10 13:11:57 +0900

    Backup
    Buckup

--- Assets/Mouri/Scriput/Roulette/Rouretto_New.cs
+++ Assets/Mouri/Scriput/Roulette/Rouretto_New.cs
@@ -195,7 +195,12 @@ public class Rouretto_New : MonoBehaviour
                 rouletteSound.rouletteSoundType = RouletteSoundType.Open;
                 rouletteSound.PlaySound();
             }
-       
+
+            if (rouletto_CG != null)
+            {
+                rouletto_CG.StartSpinEffect(0.05f);  // 0.05秒ごとに画像切替
+            }
+
 
         }
         else
@@ -246,6 +251,11 @@ public class Rouretto_New : MonoBehaviour
         {
             StopCoroutine(spinCoroutine);
 
+        }
+
+        if(rouletto_CG!= null)
+        {
+            rouletto_CG.StopSpinAndShow(jobName[(int)job]);
         }
         if(rouletteSound != null)
         {
